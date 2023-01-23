6f7afbf44506436039ee8ef7e9abf72350e0baed
sayedali1 <said.ali199859@gmail.com>
2023-01-23 18:01:53 +0200

    task1

--- printint.c
+++ printint.c
@@ -9,8 +9,36 @@
 int print_int(va_list pa, int *pCount)
 {
 	int num = va_arg(pa, int);
+	int powten = 1, i, j, n;
 
-	_putInt(num, pCount);
+	if (num != 0)
+	{
+		if (num < 0)
+		{
+			_putchar('-');
+			*pCount += 1;
+			num *= -1;
+		}
+		n = num;
+		for (i = 0; n /= 10; i++)
+		{
+			powten *= 10;
+		}
+
+		for (j = 0; j <= i; j++)
+		{
+			_putchar((num / powten) + '0');
+			*pCount += 1;
+			num %= powten;
+			powten /= 10;
+		}
+		/* _putInt(num, pCount); */
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
 
 	return (0);
 }
