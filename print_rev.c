a8b2ce7ca1fdd4a4e1296bd56af2b5ef844e4c78
sayedali1 <said.ali199859@gmail.com>
2023-01-23 20:59:28 +0200

    task13

--- print_rev.c
+++ print_rev.c
@@ -10,7 +10,19 @@ int print_rev(va_list pa, int *pCount)
 {
 	int i, j;
 	char *str = va_arg(pa, char *);
-	char *rev = malloc(sizeof(char *) * (_strlen(str)));
+	char *rev;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	rev = malloc(sizeof(char *) * (_strlen(str)));
+	if (rev == NULL)
+	{
+		free(rev);
+		return (-1);
+	}
 
 	j = 0;
 	for (i = _strlen(str) - 1; i >= 0; i--)
@@ -21,5 +33,6 @@ int print_rev(va_list pa, int *pCount)
 	_putstr(rev, _strlen(rev));
 	pCount += _strlen(rev);
 
+	free(rev);
 	return (1);
 }
