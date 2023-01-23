701d280ccd9e8d6ed5e5c7df32e4c53af2f6d93a
sayed <said.ali199859@gmail.com>
2023-01-23 10:47:30 +0200

    delte

--- main.c
+++ main.c
@@ -4,8 +4,14 @@
 
 int main(void)
 {
-    /* int len; */
-    /* len =  */_printf("%i\n", -102);
+    int len;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    _printf("Unknown:[%r]\n");
+    len = _printf("Percent:[%%]\n");
+    _printf("String:[%s]\n", "I am a string !");
+
     /* printf("%d\n",len); */
     return (0);
 }
