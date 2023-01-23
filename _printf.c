aa13f1704ca2b670d1562483578acd75cb2090ca
sayedali1 <said.ali199859@gmail.com>
2023-01-23 13:48:08 +0200

    task0

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 		{'\0', NULL}
 	};
 
-	if (format == NULL || (format[0] == '%' && format[i] == '/0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(pa, format);
